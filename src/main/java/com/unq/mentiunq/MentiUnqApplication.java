d2f2703470d9a23d936daffd8bbc876c4227f54b
Nico Alvarez (NA433420) <nicolas.alvarez@redb.ee>
2022-09-03 15:35:30 -0300

    Initial commit

---
+++ src/main/java/com/unq/mentiunq/MentiUnqApplication.java
@@ -0,0 +1,13 @@
+package com.unq.mentiunq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentiUnqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentiUnqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unq/mentiunq/MentiUnqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unq.mentiunq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentiUnqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
