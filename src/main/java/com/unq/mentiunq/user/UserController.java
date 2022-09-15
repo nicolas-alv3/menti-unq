114c1a1c8ab8717d0ca22ee42f4dfc79a91e430d
Nico Alvarez (NA433420) <nicolas.alvarez@redb.ee>
2022-09-14 21:03:11 -0300

    Refactor in userController

--- src/main/java/com/unq/mentiunq/user/UserController.java
+++ src/main/java/com/unq/mentiunq/user/UserController.java
@@ -10,17 +10,18 @@ import java.util.List;
 
 @CrossOrigin
 @RestController
-@Component(value = "LoginController")
+@Component(value = "UserController")
+@RequestMapping("/user")
 public class UserController {
     @Autowired
     private UserService userService;
 
-    @RequestMapping(method = RequestMethod.POST, value = "/user")
+    @RequestMapping(method = RequestMethod.POST, value = "/login")
     public ResponseEntity<User> login(@RequestBody User user) {
         return new ResponseEntity<>(userService.createUser(user), HttpStatus.OK);
     }
 
-    @RequestMapping(method = RequestMethod.GET, value = "/user")
+    @RequestMapping(method = RequestMethod.GET, value = "/")
     public ResponseEntity<List<User>> getLogin() {
         return new ResponseEntity<>(userService.getUsers(), HttpStatus.OK);
     }
