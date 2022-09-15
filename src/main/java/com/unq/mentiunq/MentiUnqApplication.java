66c941e9ed1161066d842200df5b475ad36693cf
Nico Alvarez (NA433420) <nicolas.alvarez@redb.ee>
2022-09-14 22:06:37 -0300

    Fix error in dialect

--- src/main/java/com/unq/mentiunq/MentiUnqApplication.java
+++ src/main/java/com/unq/mentiunq/MentiUnqApplication.java
@@ -2,8 +2,9 @@ package com.unq.mentiunq;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 public class MentiUnqApplication {
 
 	public static void main(String[] args) {
