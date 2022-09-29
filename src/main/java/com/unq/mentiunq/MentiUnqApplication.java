fbb1aa17807279740bd935d7d30269a4b8e8aa2f
luca <lhazuca@gmail.com>
2022-09-28 23:29:20 -0300

    Agregar swagger

--- src/main/java/com/unq/mentiunq/MentiUnqApplication.java
+++ src/main/java/com/unq/mentiunq/MentiUnqApplication.java
@@ -2,8 +2,10 @@ package com.unq.mentiunq;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class MentiUnqApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/unq/mentiunq/presentation/PresentationController.java
+++ src/main/java/com/unq/mentiunq/presentation/PresentationController.java
@@ -2,6 +2,7 @@ package com.unq.mentiunq.presentation;
 
 import com.auth0.json.auth.UserInfo;
 import com.unq.mentiunq.presentation.model.Presentation;
+import io.swagger.annotations.ApiOperation;
 import com.unq.mentiunq.user.UserInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -23,6 +24,7 @@ public class PresentationController {
 
     @PostMapping(value = "/")
     @ResponseBody
+    @ApiOperation(value = "Crea una presentacion")
     public Presentation createPresentation(HttpServletRequest req, @RequestBody @Valid Presentation presentation) {
         UserInfo user = userInfoService.getUserInfo(req);
         presentation.setOwnerEmail(user.getValues().get("email").toString());
@@ -37,6 +39,7 @@ public class PresentationController {
 
     @GetMapping(value = "/individual")
     @ResponseBody
+    @ApiOperation(value = "Obtiene las presentaciones del usuario conectado")
     public List<Presentation> currentUserPresentation(HttpServletRequest req) {
             UserInfo userInfo = userInfoService.getUserInfo(req);
             return presentationService.getAllPresentations(userInfo.getValues().get("email").toString());
