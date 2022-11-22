3f92f29bad8d54ef96e03c138cc616c7f7dd0063
Nico Alvarez <nicolas.alv3@gmail.com>
2022-11-22 20:19:25 -0300

    actualiza config de docker para deployar

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 sudo mvn clean install -DskipTests
 sudo mvn package -DskipTests
-sudo docker build -t  mentiunq .
+sudo docker build -t  mentiunq -f /docker/Dockerfile .
 sudo docker compose down
 sudo docker compose up
