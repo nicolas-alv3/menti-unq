a5e5463dbd7fb6bd3c3bb512c306510e6d12a8aa
luca <lhazuca@gmail.com>
2022-11-29 23:55:52 -0300

    arreglar nombre de endpoint para respuestas

--- src/main/java/com/unq/mentiunq/answers/AnswerPublicController.java
+++ src/main/java/com/unq/mentiunq/answers/AnswerPublicController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 @RestController
 @CrossOrigin("*")
-@RequestMapping("/public/answer2")
+@RequestMapping("/public/answer")
 public class AnswerPublicController {
 
     @Autowired
